2db5a2caf8bcad7ee731e4c03684ce57a560eed7
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-18 13:43:48 +0100

    #002-add_routes

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import List from './List';
+import NotFound from './NotFound';
+
 
 const Main = () => {
     return (
-        <div className='container-fluid'>
-            <h1 className='display-3'>Config works!</h1>
-        </div>
+        // <div className='container-fluid'>
+        //     <h1 className='display-3'>Config works!</h1>
+        // </div>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={List} />
+                    <Route exact path='/done' component={List} />
+                    <Route exact path='/not-done' component={List} />
+                    <Route component={NotFound} />
+                </Switch>
+            </BrowserRouter>
+        // </Provider>
     );
 };
 
