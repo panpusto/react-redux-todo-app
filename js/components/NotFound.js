6f1d2d720f63087a49e105c17d7f77bd3ee938ae
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-18 13:44:31 +0100

    #003-add_not_found_component

---
+++ js/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div class='container'>
+            <h1 class='display-3'>404 - Page Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
