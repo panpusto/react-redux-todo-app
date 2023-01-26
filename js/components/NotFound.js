aabaf6f7fdd12d8962a69be92e1022762517573a
Cezary <102999499+panpusto@users.noreply.github.com>
2023-01-26 10:13:48 +0100

    update class name

--- js/components/NotFound.js
+++ js/components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div class='container'>
-            <h1 class='display-3'>404 - Page Not Found</h1>
+            <h1 className='display-3'>404 - Page Not Found</h1>
         </div>
     );
 };
